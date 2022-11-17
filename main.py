127fe8edd949b512c3366a5d96997f3618198c7d
Tabs Goldman <thg19@ic.ac.uk>
2022-11-17 22:23:42 +0000

    Update response to accomodate score and 2 comments

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+def processVideo(url):
+    return (0.23, "Get better at cricket", "Don't play cricket")
+
+
 @app.route("/", methods=["GET"])
-def hellow_world():
-    Name = request.args.get('name', None)
-    return "Hello {}!".format(Name)
+def generate_response():
+    url = request.args.get('url', None)
+    (score, comment1, comment2) = processVideo(url)
+    return ','.join([score, comment1, comment2])
+
 
 if __name__ == "__main++":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
