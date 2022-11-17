e3925202fe291670599cc1e4e1b703e42d115818
Tabs Goldman <thg19@ic.ac.uk>
2022-11-17 22:40:50 +0000

    round score and transform to range 0-100

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 
 def processVideo(url):
-    return (0.23, "Get better at cricket", "Don't play cricket")
+    return (0.2356, "Get better at cricket", "Don't play cricket")
 
 
 @app.route("/", methods=["GET"])
 def generate_response():
     url = request.args.get('url', None)
     (score, comment1, comment2) = processVideo(url)
-    return ','.join([str(score), comment1, comment2])
+    return ','.join([str(int(score * 100)), comment1, comment2])
 
 
 if __name__ == "__main++":
